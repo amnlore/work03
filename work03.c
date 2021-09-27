ba5cf2a1f400102a9bc059ee6d3664f5a42f8d8a
myang27 <myang20@stuy.edu>
2021-09-26 21:17:20 -0400

    done

---
+++ work03.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int p1() {
+    int sum = 0;
+    int i;
+    for (i = 1; i < 1000; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+int p6() {
+    int summed_squares = 0;
+    int squared_sums = 0;
+    int i;
+    for (i = 1; i <= 100; i++) {
+        summed_squares += (i * i);
+        squared_sums += i;
+    }
+    squared_sums = (squared_sums * squared_sums);
+
+    return squared_sums - summed_squares;
+}
+
+int main(){
+    printf("Euler Problem 1: %d\n", p1());
+    printf("Euler Problem 6: %d\n", p6());
+}
